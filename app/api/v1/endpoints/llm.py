3c00beabf36644c966fa2da80b986e5f86eb83e0
thewoowon <thewoowon@naver.com>
2024-07-15 08:23:21 +0900

    feat: 엔드포인트 작업

--- app/api/v1/endpoints/llm.py
+++ app/api/v1/endpoints/llm.py
@@ -1,51 +1,109 @@
 from app.core.llm import process_message
-from fastapi import Request, APIRouter, Depends, HTTPException, status, Body
+from fastapi import Request, APIRouter, Depends, HTTPException, Body
 from sqlalchemy.orm import Session
 from fastapi.responses import StreamingResponse
-from app.db import schemas
 from app.db.database import get_session
 from app.db.models import Message, Session as SessionModel
 import uuid
 from datetime import datetime
+from typing import Optional
+import json
+from app.core.store import session_info_store, chat_history_store  # store 모듈을 가져옴
+from langchain_core.messages import HumanMessage
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from sqlalchemy.future import select
 
 router = APIRouter()
 
-store = {}
-
 
 @router.post("/execute_llm")
-async def execute_llm(request: Request, session: Session = Depends(get_session)):
+async def execute_llm(request: Request, session: Session = Depends(get_session), session_id: Optional[str] = None,):
     if not hasattr(request.state, 'user'):
         raise HTTPException(status_code=401, detail="사용자 정보를 찾을 수 없습니다.")
 
+    body = await request.body()
+    data = json.loads(body)
+    prompt = data.get("prompt")
+    session_id = data.get("session_id")
+
     user = request.state.user
-    if not request.session_id:
-        # session_id가 없으면 신규 채팅으로 간주
-        # session_id를 생성하고 store에 저장
+    if not session_id:
         use_session = SessionModel(
             user_id=user.id,
             end_time=datetime.now(),
             status="active",
-            code=uuid.uuid4(),
+            code=str(uuid.uuid4()),
         )
         session.add(use_session)
         session.commit()
 
-        store[use_session.code] = {
+        new_message = Message(
+            session_id=use_session.id,
+            user_id=user.id,
+            message_text=prompt,
+            timestamp=datetime.now(),
+            sender_type="user",
+            message_type="chat",
+        )
+        session.add(new_message)
+        session.commit()
+
+        session_info_store.set(use_session.code, {
             "user_id": user.id,
             "session_id": use_session.id,
-        }
+        })
+
+        chat_history_store.set(use_session.id, InMemoryChatMessageHistory(
+            messages=[HumanMessage(prompt)]))
 
         return {"redirect_path": f"/chat/{use_session.code}"}
 
-    prompt_text = request.prompt
-    if not prompt_text:
-        return {"error": "No prompt provided."}
-    print(f"Prompt: {prompt_text}")
-    session_id = "1"  # This can be dynamic based on user session management
+    # message_type: "chat" | "sql" | "schema";
+    # sender_type: "user" | "lens" | "system";
 
-    response = await process_message(prompt_text, session_id)
-    print(response)
+    session_obj = session.execute(
+        select(SessionModel).filter(SessionModel.code == session_id)
+    ).scalars().first()
+
+    new_message = Message(
+        session_id=session_obj.id,
+        user_id=user.id,
+        message_text=prompt,
+        timestamp=datetime.now(),
+        sender_type="user",
+        message_type="chat",
+    )
+    session.add(new_message)
+    session.commit()
+
+    sql_array = []
+    response = await process_message(prompt, session_id, sql_array)
+
+    new_gpt_message = Message(
+        session_id=session_obj.id,
+        user_id=user.id,
+        message_text=response,
+        timestamp=datetime.now(),
+        sender_type="lens",
+        message_type="chat",
+    )
+
+    session.add(new_gpt_message)
+    session.commit()
+
+    # sql_array가 있을 경우에만 db에 저장
+    if sql_array:
+        for sql in sql_array:
+            new_sql_message = Message(
+                session_id=session_obj.id,
+                user_id=user.id,
+                message_text=sql.get("query"),
+                timestamp=datetime.now(),
+                sender_type="lens",
+                message_type="sql",
+            )
+            session.add(new_sql_message)
+            session.commit()
 
     async def generate():
         yield response
