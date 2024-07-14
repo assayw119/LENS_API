8124a9133fdb4c9e03bdb2f7aaf2cd1b0f927249
YoungWoongPark <assayw119@naver.com>
2024-07-14 20:47:59 +0900

    쿼리 실행 로직 수정

--- app/api/v1/endpoints/query.py
+++ app/api/v1/endpoints/query.py
@@ -10,13 +10,14 @@ metadata_order = {"name": "Query API Version 1",
                   "description": "Version 1 Query API"}
 
 @router.post("/execute_query")
-async def execute_query(input_text: schemas.TextInput, session: Session = Depends(get_session)):
+async def execute_query(input_text: schemas.TextInput):
     query = input_text.text.strip()
     try:
         # SQL 쿼리 실행
         result = run_sql_query(query)
         # 결과를 JSON 형식으로 변환
-        return [dict(row) for row in result]
+        # return [dict(row) for row in result]
+        return result
     except Exception as e:
         error_message = str(e)
         raise HTTPException(status_code=400, detail=error_message)
