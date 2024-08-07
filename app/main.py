cdcfac1cd7c523d4124765821fddd6b957239bf3
YoungWoongPark <assayw119@naver.com>
2024-08-08 03:36:03 +0900

    llm 로직 최종

--- app/main.py
+++ app/main.py
@@ -122,7 +122,7 @@ async def verify_token_middleware(request: Request, call_next):
 
 @app.get("/")
 def root():
-    return {"message": "Hello Fastapi v38, last deploy is 2024-08-07"}
+    return {"message": "Hello Fastapi v39, last deploy is 2024-08-07"}
 
 # health check
 
